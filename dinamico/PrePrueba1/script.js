dc8c130ddf3a6d57685b10fc8230d7df86cf8ada
Jock3rlin3 <52330672+Jock3rlin3@users.noreply.github.com>
2026-06-12 13:01:38 -0300

    HOLAAA

--- dinamico/PrePrueba1/script.js
+++ dinamico/PrePrueba1/script.js
@@ -1,40 +1,49 @@
-const evento_musical=document.getElementById("evento_musical")
-evento_musical.addEventListener("change",function(){
-    if (cant_invitados>=300)
-        evento_musical.style.visibility="visible";
-    else
-        evento_musical.style.visibility="hidden";
 
-})
 const Calcular_boton = document.getElementById("Calcular")
-Calcular_boton.addEventLister("click", function () {
+Calcular_boton.addEventListener("click", function () {
 
-let errores=""
-let valor_reserva=0
-const resultados=document.getElementById("resultados")
-const cant_invitados=document.getElementById("cant_invitados")
-const nombre=document.getElementById("nombre")
-const size_salon=document.getElementById("size_salon")
+    const div_reserva = document.getElementById("resultados")
+    const cant_invitados = document.getElementById("cant_invitados")
+    const nombre = document.getElementById("nombre")
+    const valor_salon = document.getElementById("size_salon").value
+    const evento_musical = document.getElementById("evento_musical")
+    let errores = ""
+    let valor_reserva=valor_salon
+    if (!Number.isNaN(Number(nombre.value)))
+        errores="El nombre no puede tener numeros.<br>";
+    if (evento_musical.checked)
+        valor_reserva=valor_reserva*1.3;
+    
 
+    if (errores=="")
+    div_reserva.innerHTML = "El valor de la reserva es: "+valor_reserva+"<br>Y el valor de la senia es: "+valor_reserva*0.35;
+    else
+    div_reserva.innerHTML = errores;
+        
 
-resultados.innerHTML=valor_reserva
+})
+const div_evento = document.getElementById("div_evento")
+cant_invitados.addEventListener("change", function () {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (Number(cant_invitados.value) >= 300)
+        div_evento.style.visibility = "visible";
+    else
+        div_evento.style.visibility = "hidden";
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
